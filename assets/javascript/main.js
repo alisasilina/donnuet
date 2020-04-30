871627559d20aea811455a0a002dcf1bbd039a5c
Alisa Silina <alisasilina@gmail.com>
2020-05-01 00:46:37 +0300

    details toggle fixed

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,14 +1,6 @@
-const detailsButtons = document.querySelectorAll('.voted .details__title');
-const detailsDivs = document.querySelectorAll('.details__list');
+// Details Div Toggle
+function detailsToggle(event) {
+  const detailsDiv = event.querySelector('ol');
+  detailsDiv.classList.toggle("d-none");
+}
 
-detailsButtons[0].addEventListener('click', e => {
-  detailsDivs[0].classList.toggle('d-none');
-});
-
-detailsButtons[1].addEventListener('click', e => {
-  detailsDivs[1].classList.toggle('d-none');
-});
-
-detailsButtons[2].addEventListener('click', e => {
-  detailsDivs[2].classList.toggle('d-none');
-});
